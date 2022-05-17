89fd1d0083dc3d6a0ac14e8bbbdeacc543c63524
devgis <devgis@163.com>
2022-05-17 20:14:00 +0800

    Public
    Public

---
+++ Source/PagerControl/FormMain.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace PagerControl
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvList = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvList)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvList
+            // 
+            this.dgvList.AllowUserToAddRows = false;
+            this.dgvList.AllowUserToDeleteRows = false;
+            this.dgvList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvList.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvList.Location = new System.Drawing.Point(0, 0);
+            this.dgvList.Name = "dgvList";
+            this.dgvList.ReadOnly = true;
+            this.dgvList.RowTemplate.Height = 23;
+            this.dgvList.Size = new System.Drawing.Size(791, 262);
+            this.dgvList.TabIndex = 1;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(791, 262);
+            this.Controls.Add(this.dgvList);
+            this.Name = "FormMain";
+            this.Text = "FormMain";
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvList)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvList;
+    }
+}
---
+++ Source/PagerControl/FormMain.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace PagerControl
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+        bool bexit = false;
+        int rowcount = 4; //默认数据行数
+        int sleeptime = 3000; //间隔时间 mm;
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            ThreadPool.QueueUserWorkItem(o =>
+            {
+                while (!bexit)
+                {
+                    DataTable dt = SQLHelper.Instance.GetDataTable("select * from t_JQ");
+                    if (dt != null && dt.Rows.Count > 0)
+                    {
+                        //dgvList.DataSource = dt;
+                        int start = 0;
+                        while (true && !bexit)
+                        {
+                            if (start >= dt.Rows.Count)
+                            {
+                                break;
+                            }
+                            DataTable dtNew = dt.Clone();
+                            List<DataRow> list = new List<DataRow>();
+                            for (int i = start; i < start + rowcount && i < dt.Rows.Count; i++)
+                            {
+                                DataRow dr = dtNew.NewRow();
+                                CopyDataRow(dt.Rows[i], dr);
+                                dtNew.Rows.Add(dr);
+                                list.Add(dr);
+                            }
+                            ShowLed(list);
+                            dgvList.Invoke((EventHandler)(delegate
+                            {
+                                dgvList.DataSource = dtNew;
+
+                            }));
+
+                            start += rowcount;
+                            Thread.Sleep(sleeptime);
+                        }
+                    }
+                    Thread.Sleep(200);
+                }
+
+            }, 0);
+            
+        }
+        private void CopyDataRow(DataRow sourceRow, DataRow destRow)
+        {
+            if (sourceRow == null || destRow == null)
+                return;
+            foreach (DataColumn aDataColumn in sourceRow.Table.Columns)
+            {
+                try
+                {
+                    destRow[aDataColumn.ColumnName] = sourceRow[aDataColumn.ColumnName];
+                }
+                catch
+                { }
+            }
+        }
+
+        public void ShowLed(List<DataRow> list)
+        {
+            //读取List 里边的行数显示在Led
+        }
+    }
+}
